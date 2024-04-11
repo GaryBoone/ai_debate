1b97976dc1c665d4116ee550d8f67a47f6dba6a5
Gary Boone <gary.boone@gmail.com>
2024-04-10 18:36:35 -0700

    switched to downloaded libraries in CMakeLists.txt; switched to doctest

--- tests/chunk_processor_test.cpp
+++ tests/chunk_processor_test.cpp
@@ -1,80 +1,80 @@
-#include <catch2/catch_test_macros.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
 
 #define private public
 #include "../src/gpt_client/chunk_processor.h"
 
-TEST_CASE("extractDataSections extracts valid examples",
-          "[extractDataSections]") {
+TEST_CASE("extractDataSections extracts valid examples") {
   GptChunkProcessor cp;
 
   std::vector<std::string> sections = cp._extractDataSections("data: foo");
-  REQUIRE(sections == std::vector<std::string>{"foo"});
+  CHECK(sections == std::vector<std::string>{"foo"});
 
   // Allowed: 0 or more spaces after "data:"
   sections = cp._extractDataSections("data:foo");
-  REQUIRE(sections == std::vector<std::string>{"foo"});
+  CHECK(sections == std::vector<std::string>{"foo"});
 
   // Allowed: Only one linefeed between data sections.
   sections = cp._extractDataSections("data: foo\ndata: bar\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar"});
 
   // Allowed: 0 or more spaces before and after "data:"
   sections = cp._extractDataSections("\n data:  foo");
-  REQUIRE(sections == std::vector<std::string>{"foo"});
+  CHECK(sections == std::vector<std::string>{"foo"});
 
   sections = cp._extractDataSections("\n\ndata: foo");
-  REQUIRE(sections == std::vector<std::string>{"foo"});
+  CHECK(sections == std::vector<std::string>{"foo"});
 
   // Note: Spaces after the data section are returned.
   sections = cp._extractDataSections("\n\ndata: foo   ");
-  REQUIRE(sections == std::vector<std::string>{"foo   "});
+  CHECK(sections == std::vector<std::string>{"foo   "});
 
   // Empty data section is allowed.
   sections = cp._extractDataSections("data: ");
-  REQUIRE(sections == std::vector<std::string>{""});
+  CHECK(sections == std::vector<std::string>{""});
 
   sections = cp._extractDataSections("data: foo\n");
-  REQUIRE(sections == std::vector<std::string>{"foo"});
+  CHECK(sections == std::vector<std::string>{"foo"});
 
   sections = cp._extractDataSections("data: foo\n\ndata: bar\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar"});
 
   sections = cp._extractDataSections("data: foo\n\ndata: bar\n\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar"});
 
   sections = cp._extractDataSections("data: foo\n\ndata: bar\n\ndata: baz\n\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar", "baz"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar", "baz"});
 
   sections = cp._extractDataSections("data: foo\n\ndata: bar\n\ndata: baz\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar", "baz"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar", "baz"});
 
   sections = cp._extractDataSections("data: foo\n\ndata: bar\n\ndata: baz");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar", "baz"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar", "baz"});
 
   sections = cp._extractDataSections("\ndata: foo\n\ndata: bar\n\ndata: baz\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar", "baz"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar", "baz"});
 
   sections =
       cp._extractDataSections("\n\ndata: foo\n\ndata: bar\n\ndata: baz\n\n");
-  REQUIRE(sections == std::vector<std::string>{"foo", "bar", "baz"});
+  CHECK(sections == std::vector<std::string>{"foo", "bar", "baz"});
 }
 
-TEST_CASE("extractDataSections invalid examples", "[extractDataSections]") {
+TEST_CASE("extractDataSections invalid examples") {
   GptChunkProcessor cp;
 
   std::vector<std::string> sections = cp._extractDataSections("dat: foo");
-  REQUIRE(sections == std::vector<std::string>{});
+  CHECK(sections == std::vector<std::string>{});
 
   sections = cp._extractDataSections("data: foo\nbar");
-  REQUIRE(sections == std::vector<std::string>{"foo"});
+  CHECK(sections == std::vector<std::string>{"foo"});
 }
 
-TEST_CASE("An example test case", "[example]") {
-  REQUIRE(1 == 1); // An example test that always passes
-  //   REQUIRE(
+TEST_CASE("An example test case") {
+  CHECK(1 == 1); // An example test that always passes
+  //   CHECK(
   //       cp.parse_chunk_data(
   //           R"({"choices":[{"index":0,"delta":{"role":"assistant","content":"foo"},"logprobs":null,"finish_reason":null}]})")
   //           ==
   //       true);
-  //   REQUIRE(1 == 1);
+  //   CHECK(1 == 1);
 }
